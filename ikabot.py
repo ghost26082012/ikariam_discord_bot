fd886951daa29900f2a9770405710de02d5f471a
ghost26082012 <44878635+ghost26082012@users.noreply.github.com>
2018-11-08 16:19:57 -0500

    Update ikabot.py

--- ikabot.py
+++ ikabot.py
@@ -9,20 +9,15 @@ async def on_ready():
     print('Logged in as')
     print(bot.user.name)
     print('Enter your command')
-
-async def on_message(message):
-    if message.content.startswith("!greet"):
-        userID = message.author.id
-        await bot.send_message(message.channel, "Hello there !!!")
-    if message.content.startswith("!help"):
-        userID = message.author.id
-        await bot.send_message(message.channel, "Help !!!")
                                   
 @bot.command()
 ### Description of command goes here.
 async def hello():
-    await bot.say('Hello')
-                                 
+    await bot.say('Hello!!')
+
+### Description of command goes here.
+async def greet():
+    await bot.say('Greeting!!')
 ######################################################################
 
 bot.run('NTA5NDg0MDc3ODkyNzYzNjQ4.DsYfWw.eveh1J-vwsXN3kZbXZyk2_gHu18')
