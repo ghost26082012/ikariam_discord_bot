d575f032ace8db1d8d0ad110f29d559a61bf7d32
ghost26082012 <44878635+ghost26082012@users.noreply.github.com>
2018-11-08 16:23:50 -0500

    Update ikabot.py

--- ikabot.py
+++ ikabot.py
@@ -15,9 +15,11 @@ async def on_ready():
 async def hello():
     await bot.say('Hello!!')
 
+@bot.command()
 ### Description of command goes here.
 async def greet():
     await bot.say('Greeting!!')
+    
 ######################################################################
 
 bot.run('NTA5NDg0MDc3ODkyNzYzNjQ4.DsYfWw.eveh1J-vwsXN3kZbXZyk2_gHu18')
