4bc51e3230b3c8d7046292e18597f0afadfc5666
ghost26082012 <44878635+ghost26082012@users.noreply.github.com>
2018-11-08 15:35:25 -0500

    Update ikabot.py

--- ikabot.py
+++ ikabot.py
@@ -13,5 +13,14 @@ async def on_ready():
 @bot.command()
 async def greet(ctx):
     await ctx.send(":smiley: :wave: Hello, there!")
+    
+# This tell the Interpreter that this function is a command for discord
+@client.command(name="greet") # 'name' is literaly the name of the command
+                              # this is what you type after the prefix
+async def exampleCommand(): # commands can also take paramenters this example takes none
+                            # but if it does have paramenter when the command is called it'll need
+                            # them or else the command won't work
+  
+  await client.say("Hello there!!")
 
 bot.run('NTA5NDg0MDc3ODkyNzYzNjQ4.DsYfWw.eveh1J-vwsXN3kZbXZyk2_gHu18')
